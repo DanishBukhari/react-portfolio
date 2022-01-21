f852bce05c4e762bafaf52cb6627061dc2c6460b
Danish <dani.syed.18@gmail.com>
2022-01-22 02:39:12 +0500

    initial commit

---
+++ client/build/precache-manifest.468cde4fef724a14219c41ea47535123.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7423c9d5e174dcdcadf391baf2a68f7",
+    "url": "/react-portfolio-web/index.html"
+  },
+  {
+    "revision": "90db485a11523cad2317",
+    "url": "/react-portfolio-web/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "8da42cf1d04e1dd6430b",
+    "url": "/react-portfolio-web/static/css/main.ad3080fd.chunk.css"
+  },
+  {
+    "revision": "90db485a11523cad2317",
+    "url": "/react-portfolio-web/static/js/2.157925e6.chunk.js"
+  },
+  {
+    "revision": "8bcdfbedbaedaebc1be8002ea83beeb7",
+    "url": "/react-portfolio-web/static/js/2.157925e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8da42cf1d04e1dd6430b",
+    "url": "/react-portfolio-web/static/js/main.16405cc1.chunk.js"
+  },
+  {
+    "revision": "8af4c8cf774abbee0642",
+    "url": "/react-portfolio-web/static/js/runtime-main.06461280.js"
+  },
+  {
+    "revision": "a3e0b5f427803a187c1b62c5919196aa",
+    "url": "/react-portfolio-web/static/media/Poppins-Bold.a3e0b5f4.ttf"
+  },
+  {
+    "revision": "544fa4f2678a8285eb88b8dfe503c90c",
+    "url": "/react-portfolio-web/static/media/Poppins-ExtraBold.544fa4f2.ttf"
+  },
+  {
+    "revision": "f6ea751e936ade6edcd03a26b8153b4a",
+    "url": "/react-portfolio-web/static/media/Poppins-Light.f6ea751e.ttf"
+  },
+  {
+    "revision": "f61a4eb27371b7453bf5b12ab3648b9e",
+    "url": "/react-portfolio-web/static/media/Poppins-Medium.f61a4eb2.ttf"
+  },
+  {
+    "revision": "8b6af8e5e8324edfd77af8b3b35d7f9c",
+    "url": "/react-portfolio-web/static/media/Poppins-Regular.8b6af8e5.ttf"
+  },
+  {
+    "revision": "4cdacb8f89d588d69e8570edcbe49507",
+    "url": "/react-portfolio-web/static/media/Poppins-SemiBold.4cdacb8f.ttf"
+  },
+  {
+    "revision": "25cd0f688f815bc4f6ac2b71eb6278ba",
+    "url": "/react-portfolio-web/static/media/Poppins-Thin.25cd0f68.ttf"
+  },
+  {
+    "revision": "f7b5182ca49f5dba70175926f3a1e5af",
+    "url": "/react-portfolio-web/static/media/education.f7b5182c.svg"
+  },
+  {
+    "revision": "f38cb5bef3331fc11f19a170e83e488f",
+    "url": "/react-portfolio-web/static/media/im4.f38cb5be.jpg"
+  },
+  {
+    "revision": "f0d8135df77bc0f4d96428a73f01c336",
+    "url": "/react-portfolio-web/static/media/interests.f0d8135d.svg"
+  },
+  {
+    "revision": "ff7ad67d406e4807e7f6c925e5a6b3ac",
+    "url": "/react-portfolio-web/static/media/me.ff7ad67d.jpg"
+  },
+  {
+    "revision": "55bce9b573515cd8847680bf79a1c173",
+    "url": "/react-portfolio-web/static/media/profilephoto.55bce9b5.jpg"
+  },
+  {
+    "revision": "4f981aecfc9853db5215d80c6dca1220",
+    "url": "/react-portfolio-web/static/media/programming-skills.4f981aec.svg"
+  },
+  {
+    "revision": "aa1f39c4a0c6c017d061958b9a4d94f3",
+    "url": "/react-portfolio-web/static/media/projects.aa1f39c4.svg"
+  },
+  {
+    "revision": "2eb3f531d5630506b0db44825dc9c973",
+    "url": "/react-portfolio-web/static/media/shape-bg.2eb3f531.png"
+  },
+  {
+    "revision": "d9d994da1a70662634ff9d62327ec0f6",
+    "url": "/react-portfolio-web/static/media/work-history.d9d994da.svg"
+  }
+]);
